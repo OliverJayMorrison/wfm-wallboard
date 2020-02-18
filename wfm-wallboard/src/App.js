fe7a7d2c31c19e34e0e8fddc5bdfd3285bedbf39
Oliver Morrison <oliverjaymorrison@gmail.com>
2020-02-18 13:36:07 +0000

    added API comments

--- wfm-wallboard/src/App.js
+++ wfm-wallboard/src/App.js
@@ -18,3 +18,9 @@ export default App
 
 //http://localhost:7001/api/wfm/main/v1/queues/1589/statistics?filter%5BfromDate%5D=2020-03-23T00%3A00%3A00.000Z&filter%5BtoDate%5D=2020-03-23T00%3A15%3A00.000Z
 //API request format
+
+//https://pusher.com/tutorials/consume-restful-api-react
+//Consume API data
+
+//https://riptutorial.com/reactjs/example/22111/http-get-request
+//API request format
